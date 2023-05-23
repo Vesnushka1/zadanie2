20c515a853a48e8eb403f83993ca364f4aab7afb
ГуськоваЕО <katya.lisina.04@list.ru>
2023-05-23 18:07:06 +0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      persons:[]
+    }
+  },
+  methods:{
+    async getPersons(){
+      let result = await fetch('https://hp-api.onrender.com/api/characters')
+      this.persons = await result.json()
+    }
+  },
+  mounted() {
+    this.getPersons()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
